44d8948dbc512a01f0b6009d31e675649a1c213b
rhys <zane1233@163.com>
2026-02-03 23:30:29 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -1,5 +1,6 @@
 from string import digits
 import math
+import random
 
 def huiwen1():
     '''
@@ -630,5 +631,48 @@ def excel20():
     输入：columnNumber = 2147483647
     输出："FXSHRXW"
     """
+    a=int(input('输入整数:'))
+    m=[]
+    h={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',
+       8:'H',9:'I',10:'J',11:'K',12:'L',13:'M',14:'N',
+       15:'O',16:'P',17:'Q',18:'R',19:'S',20:'T',21:'U',
+       22:'V',23:'W',24:'X',25:'Y',26:'Z'}
+    while True:
+        if a>26:
+            x=a%26
+            a=a//26
+            m.append(h[x])
+            continue
+        else:
+            m.append(h[a])
+            break
+    m.reverse()
+    p=''.join(m)
+    print(p)
+def caishu21():
+    """
+    猜数字大小：0-100
+    """
+    num=random.randint(1,101)
+    a=int(input("输入一个0到100的整数:"))
+    max=100;min=0
+    while True:
+        if a==num:
+            print("猜对了！！！")
+            break
+        if a>100 or a<0:
+            print("范围错了，是0到100！！！")
+            a=int(input("请重新输入一个数："))
+            continue
+        if a>num:
+            max=a
+            print(f"{min}到{max}")
+            a=int(input("继续输入："))
+            continue
+        if a<num:
+            min=a
+            print(f"{min}到{max}")
+            a=int(input("继续输入："))
+            continue
 if __name__=='__main__':
-    yici19()
+    caishu21()
