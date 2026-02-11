eb2f2eac4bdfe41f0afd3c8a3fb27009082c6c44
rhys <zane1233@163.com>
2026-02-11 22:29:34 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -887,5 +887,62 @@ def cuntong26():
         print('true')
     else:
         print('false')
+def huizong27():
+    """
+    给定一个  无重复元素 的 有序 整数数组 nums 。
+    区间 [a,b] 是从 a 到 b（包含）的所有整数的集合。
+    返回 恰好覆盖数组中所有数字 的 最小有序 区间范围列表 。
+    也就是说，nums 的每个元素都恰好被某个区间范围所覆盖，
+    并且不存在属于某个区间但不属于 nums 的数字 x 。
+    列表中的每个区间范围 [a,b] 应该按如下格式输出：
+    "a->b" ，如果 a != b
+    "a" ，如果 a == b
+
+    示例 1：
+    输入：nums = [0,1,2,4,5,7]
+    输出：["0->2","4->5","7"]
+    解释：区间范围是：
+    [0,2] --> "0->2"
+    [4,5] --> "4->5"
+    [7,7] --> "7"
+
+    示例 2：
+    输入：nums = [0,2,3,4,6,8,9]
+    输出：["0","2->4","6","8->9"]
+    解释：区间范围是：
+    [0,0] --> "0"
+    [2,4] --> "2->4"
+    [6,6] --> "6"
+    [8,9] --> "8->9"
+    """
+    exec(input("格式： nums = 列表 :"), globals())
+    print('["',end='')
+    d=0
+    for i in range(len(nums)):
+        if i==len(nums)-1:
+            if d==0 or d==1:
+                print(f'{nums[-1]}',end='')
+            if d==-1:
+                print(f'->{nums[-1]}',end='')
+            break
+        if d==0:
+            print(f'{nums[i]}',end='')
+            d=-1
+        if d==-1:
+            if nums[i+1]==nums[i]+1:
+                print("->",end='')
+                d=1
+                continue
+            else:
+                print('","',end='')
+                d=0
+                continue
+        if d==1:
+            if nums[i+1]==nums[i]+1:
+                continue
+            else:
+                print(f'{nums[i]}","',end='')
+                d=0
+    print('"]',end='')
 if __name__=='__main__':
-    cuntong26()
+    huizong27()
