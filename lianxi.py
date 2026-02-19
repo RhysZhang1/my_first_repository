982a7015b73f6cf4fead6a4d348cd8f0d45c7fa4
rhys <zane1233@163.com>
2026-02-19 23:30:01 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -1443,6 +1443,52 @@ def luoma42():
         d[3]='IX'
     luo=''.join(d)
     print(luo)
+def sanhe43():
+    """
+    给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]]
+    满足 i != j、i != k 且 j != k ，
+    同时还满足 nums[i] + nums[j] + nums[k] == 0
+    请你返回所有和为 0 且不重复的三元组。
+    注意：答案中不可以包含重复的三元组。
+
+    示例 1：
+    输入：nums = [-1,0,1,2,-1,-4]
+    输出：[[-1,-1,2],[-1,0,1]]
+    解释：
+    nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+    nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+    nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+    不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+    注意，输出的顺序和三元组的顺序并不重要。
+
+    示例 2：
+    输入：nums = [0,1,1]
+    输出：[]
+    解释：唯一可能的三元组和不为 0 。
+
+    示例 3：
+    输入：nums = [0,0,0]
+    输出：[[0,0,0]]
+    解释：唯一可能的三元组和为 0 。
+    """
+    exec(input("格式： nums = 数组  ："),globals())
+    jg=[];h=0;y=0;c=[]
+    for i in range(len(nums)-1):
+        for j in range(i+1,len(nums)):
+            b=nums[:]
+            h=nums[i]+nums[j]
+            y=0-h
+            if y not in nums:
+                continue
+            b.remove(nums[i]);b.remove(nums[j])
+            if y not in b:
+                continue
+            c.append(nums[i])
+            c.append(nums[j])
+            c.append(y)
+            c.sort()
+            jg.append(c)
+    print(jg)
 if __name__=='__main__':
     '''37 Z 暂弃'''
-    luoma42()
+    sanhe43()
