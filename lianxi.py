bc4511b67c7e02c2680a6b725eebb2d9ed8b6af8
rhys <zane1233@163.com>
2026-03-09 23:41:18 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -1670,5 +1670,54 @@ def shengkuo47():
     a2=list(a2)
     a2.sort()
     print(a2)
+def chongpai48():
+    '''
+    整数数组的一个 排列  就是将其所有成员以序列或线性顺序排列。
+
+    例如，arr = [1,2,3] ，以下这些都可以视作 arr 的排列：[1,2,3]、[1,3,2]、[3,1,2]、[2,3,1] 。
+    整数数组的 下一个排列 是指其整数的下一个字典序更大的排列。
+    更正式地，如果数组的所有排列根据其字典顺序从小到大排列在一个容器中，
+    那么数组的 下一个排列 就是在这个有序容器中排在它后面的那个排列。
+    如果不存在下一个更大的排列，那么这个数组必须重排为字典序最小的排列（即，其元素按升序排列）。
+
+    例如，arr = [1,2,3] 的下一个排列是 [1,3,2] 。
+    类似地，arr = [2,3,1] 的下一个排列是 [3,1,2] 。
+    而 arr = [3,2,1] 的下一个排列是 [1,2,3] ，因为 [3,2,1] 不存在一个字典序更大的排列。
+    给你一个整数数组 nums ，找出 nums 的下一个排列。
+
+    示例 1：
+    输入：nums = [1,2,3]
+    输出：[1,3,2]
+
+    示例 2：
+    输入：nums = [3,2,1]
+    输出：[1,2,3]
+
+    示例 3：
+    输入：nums = [1,1,5]
+    输出：[1,5,1]
+    '''
+    nums=eval(input())
+    l=len(nums)
+    x=l-1
+    for i in range(l-1):
+        if nums[x]>nums[x-1]:
+            break
+        x-=1
+    if x==0:
+        nums.sort()
+        print(nums)
+        return
+    zz=nums[x-1:]
+    zz.sort()
+    xun=(zz.index(nums[x-1]))+1
+    while True:
+        if zz[xun]>zz[xun-1]:
+            break
+        xun+=1
+    zzz=zz[:xun]+zz[xun+1:]
+    zzz.sort()
+    xin=nums[:x-1]+[zz[xun]]+zzz
+    print(xin)
 if __name__=='__main__':
-    shengkuo47()
+    chongpai48()
