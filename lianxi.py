14c2423126becabafcf7caed52eb709f2a90b431
rhys <zane1233@163.com>
2026-03-24 23:29:29 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -2119,14 +2119,95 @@ def tiaoyue59():
                     return
                 if n-m<nums[m]:
                     break
-            if n-m==nums[m]:
+            if n-m>=nums[m]:
                 m-=1
                 continue
-            if n-m>nums[m]:
-                print('false')
-                return
             if n-m<nums[m]:
                 break
     print('true')
+def hebing60():
+    """
+    以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+    请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+
+    示例 1：
+    输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+    输出：[[1,6],[8,10],[15,18]]
+    解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+
+    示例 2：
+    输入：intervals = [[1,4],[4,5]]
+    输出：[[1,5]]
+    解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+
+    示例 3：
+    输入：intervals = [[4,7],[1,4]]
+    输出：[[1,7]]
+    解释：区间 [1,4] 和 [4,7] 可被视为重叠区间。
+    """
+    nums=eval(input())
+    if len(nums)<=1:
+        print(nums)
+        return
+    nums.sort()
+    j=[]
+    b=0
+    def two(nums):
+        if nums[0][1]<nums[1][0]:
+            return nums
+        if nums[0][1]>nums[1][1]:
+            c=[]
+            c.append(nums[0])
+            return c
+        else:
+            c=[]
+            c.append(nums[0][0])
+            c.append(nums[1][1])
+            zzz=[]
+            zzz.append(c)
+            return zzz
+    if len(nums)==2:
+        j=two(nums)
+        print(j)
+        return
+    while True:
+        if b==len(nums)-1:
+            j.append(nums[b])
+            break
+        if b==len(nums)-2:
+            c=nums[-2:]
+            cc=two(c)
+            for i in cc:
+                j.append(i)
+            break
+        f=nums[b][0]
+        if not nums[b][1]>=nums[b+1][0]:
+            j.append(nums[b])
+            b+=1
+            continue
+        if nums[b][1]>nums[b+1][1]:
+            max=nums[b][1]
+        else:
+            max=nums[b+1][1]
+        b+=2
+        while True:
+            if b==len(nums):
+                x=[]
+                x.append(f)
+                x.append(max)
+                j.append(x)
+                break
+            if nums[b][0]>max:
+                x=[]
+                x.append(f)
+                x.append(max)
+                j.append(x)
+                break
+            if nums[b][1]>max:
+                max=nums[b][1]
+            b+=1
+        if b==len(nums):
+            break
+    print(j)
 if __name__=='__main__':
-    tiaoyue59()
+    hebing60()
