2992f6979e8a921a500d896c428bcd96c14ebd61
rhys <zane1233@163.com>
2026-03-30 23:42:25 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -2314,5 +2314,25 @@ def yidong63():
     z2=math.comb(m+n,m)
     z3=math.comb(x-1-m+y-1-n,x-1-m)
     print(z1-z2*z3)
+def luhe64():
+    """
+    给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+    说明：每次只能向下或者向右移动一步。
+
+    示例 1：
+    输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+    输出：7
+    解释：因为路径 1→3→1→1→1 的总和最小。
+
+    示例 2：
+    输入：grid = [[1,2,3],[4,5,6]]
+    输出：12
+    """
+    nums=eval(input())
+    x=len(nums)
+    y=len(nums[0])
+    m=math.comb(x+y-2,x-1)
+    for i in range(m):
+
 if __name__=='__main__':
-    yidong63()
+    luhe64()
