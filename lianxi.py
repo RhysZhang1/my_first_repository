404b1b7c65171cfad5dde26d81354bce2f00f522
rhys <zane1233@163.com>
2026-03-31 23:54:21 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -2316,7 +2316,7 @@ def yidong63():
     print(z1-z2*z3)
 def luhe64():
     """
-    给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+    给定一个包含非负整数的 m x n 的用二维数组表示的网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
     说明：每次只能向下或者向右移动一步。
 
     示例 1：
@@ -2328,11 +2328,19 @@ def luhe64():
     输入：grid = [[1,2,3],[4,5,6]]
     输出：12
     """
-    nums=eval(input())
-    x=len(nums)
-    y=len(nums[0])
-    m=math.comb(x+y-2,x-1)
-    for i in range(m):
-
+    grid=eval(input())
+    m=len(grid)
+    n=len(grid[0])
+    dp=[[0]*n for _ in range(m)]
+    dp[0][0]=grid[0][0]
+    for j in range(1,n):
+        dp[0][j]=dp[0][j-1]+grid[0][j]
+    for i in range(1,m):
+        dp[i][0]=dp[i-1][0]+grid[i][0]
+    for i in range(1,m):
+        for j in range(1,n):
+            dp[i][j]=min(dp[i-1][j],dp[i][j-1])+grid[i][j]
+    print(dp[-1][-1])
+    
 if __name__=='__main__':
     luhe64()
