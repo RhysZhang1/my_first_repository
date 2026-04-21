830c39404be110ea0a956d9428e46f7bbfefebef
rhys <zane1233@163.com>
2026-04-21 20:10:09 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -3169,5 +3169,36 @@ def jiayou75():
             return
         i += 1
     print(-1)
+def yici76():
+    """
+    给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。
+    你必须设计并实现线性时间复杂度的算法且使用常数级空间来解决此问题。
+
+    示例 1：
+    输入：nums = [2,2,3,2]
+    输出：3
+
+    示例 2：
+    输入：nums = [0,1,0,1,0,1,99]
+    输出：99
+    """
+    def singleNumber(self, nums: List[int]) -> int:
+        # a=set()
+        # b=set()
+        # x=0
+        # for i in nums:
+        #     a.add(i)
+        #     if len(a)==x:
+        #         b.add(i)
+        #     x=len(a)
+        # return (a-b).pop()    #O(n);O(n)
+
+        # return int((sum(set(nums))*3-sum(nums))/2)        #O(n);O(n)
+
+        a=0;b=0
+        for i in nums:
+            a=(a ^ i) & ~b
+            b=(b ^ i) & ~a
+        return a                      #O(n);O(1)
 if __name__=='__main__':
     jiayou75()
