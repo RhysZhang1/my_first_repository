8b5f7396fd5ce6c0995a9db7f55dcd7c99f5ea50
rhys <zane1233@163.com>
2026-05-14 18:48:07 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -4588,6 +4588,69 @@ def nengliang97():
         if z-c<nu[-1][1]:
             z=nu[-1][1]+c
         return z
+def hubu98():
+    """
+    给你一个长度为 偶数 n 的整数数组 nums 和一个整数 limit 。
+    每一次操作，你可以将 nums 中的任何整数替换为 1 到 limit 之间的另一个整数。
+    如果对于所有下标 i（下标从 0 开始），nums[i] + nums[n - 1 - i] 都等于同一个数，则数组 nums 是 互补的 。
+    例如，数组 [1,2,3,4] 是互补的，因为对于所有下标 i ，nums[i] + nums[n - 1 - i] = 5 。
+    返回使数组 互补 的 最少 操作次数。
 
+    示例 1:
+    输入：nums = [1,2,4,3], limit = 4
+    输出：1
+    解释：经过 1 次操作，你可以将数组 nums 变成 [1,2,2,3]（加粗元素是变更的数字）：
+    nums[0] + nums[3] = 1 + 3 = 4.
+    nums[1] + nums[2] = 2 + 2 = 4.
+    nums[2] + nums[1] = 2 + 2 = 4.
+    nums[3] + nums[0] = 3 + 1 = 4.
+    对于每个 i ，nums[i] + nums[n-1-i] = 4 ，所以 nums 是互补的。
+
+    示例 2：
+    输入：nums = [1,2,2,1], limit = 2
+    输出：2
+    解释：经过 2 次操作，你可以将数组 nums 变成 [2,2,2,2] 。你不能将任何数字变更为 3 ，因为 3 > limit 。
+
+    示例 3：
+    输入：nums = [1,2,1,2], limit = 2
+    输出：0
+    解释：nums 已经是互补的。
+    """
+    def minMoves(self, nums: List[int], limit: int) -> int:
+        # n=len(nums)
+        # l=n//2
+        # n1=nums[:l]
+        # n2=nums[len(nums)-1:l-1:-1]
+        # n3=[n1[i]+n2[i] for i in range(l)]
+        # jj=[]
+        # for s in range(min(n3),max(n3)+1):
+        #     z=0
+        #     for i in range(l):
+        #         x=n3[i]
+        #         if x!=s:
+        #             if 1<=s-n1[i]<=limit or 1<=s-n2[i]<=limit:
+        #                 z+=1
+        #             else:
+        #                 z+=2
+        #     jj.append(z)
+        # return min(jj)
+
+        n=len(nums)
+        d=[0 for i in range(2*limit+2)]
+        for i in range(n//2):
+            a=min(nums[i],nums[n-1-i])
+            b=max(nums[i],nums[n-1-i])
+            d[2]+=2
+            d[a+1]-=1
+            d[a+b]-=1
+            d[a+b+1]+=1
+            d[b+limit+1]+=1
+        mn=n
+        c=0
+        for i in range(2,2*limit+1):
+            c+=d[i]
+            if c<mn:
+                mn=c
+        return mn
 if __name__=='__main__':
     zuida84()
