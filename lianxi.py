fb29f910246100d7a6c3b08dba98f329ef34586e
rhys <zane1233@163.com>
2026-05-16 23:20:24 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -4730,5 +4730,69 @@ def jisuan99():
                     continue
                 i+=1
         return int(s)
+def chazhao100():
+    """
+    编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+    每行的元素从左到右升序排列。
+    每列的元素从上到下升序排列。
+
+    示例 1：
+    输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+    输出：true
+
+    示例 2：
+    输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+    输出：false
+    """
+
+    def searchMatrix(matrix: List[List[int]], target: int) -> bool:
+        # x=len(matrix)
+        # y=len(matrix[0])
+        # l=0
+        # u=0
+        # d=-1
+        # r=-1
+        # for i in range(x):
+        #     l=i
+        #     if matrix[i][0]>target:
+        #         l+=1
+        #         break
+        # for i in range(y):
+        #     u=i
+        #     if matrix[0][i]>target:
+        #         u+=1
+        #         break
+        # for i in range(l):
+        #     if matrix[i][u-1]>=target:
+        #         break
+        #     r=i
+        # for i in range(u):
+        #     if matrix[l-1][i]>=target:
+        #         break
+        #     d=i
+        # mx=[i[d+1:u] for i in matrix[r+1:l]]
+        # mx=str(mx)
+        # mx=mx[1:len(mx)-1]
+        # mx.replace('[','')
+        # mx.replace(']','')
+        # mx='['+mx+']'
+        # mx=list(mx)
+        # if target in mx:
+        #     return True
+        # else:
+        #     return False
+
+        if not matrix or not matrix[0]:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        row, col = 0, n - 1
+        while row < m and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                col -= 1
+            else:
+                row += 1
+        return False
 if __name__=='__main__':
     zuida84()
